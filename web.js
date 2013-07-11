56370d405878bee398be7efe41a8c2c62398edca
Jose Gonzalez <jcgzzc@gmail.com>
2013-07-11 22:15:33 +0000

    changed to inside response.send()

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var bufIndex = fs.readFileSync("./index.html");
-  var strIndex = bufIndex.toString();
+  var fs = require('fs');
 
-  response.send();
+  var bufIndex = new Buffer(fs.readFileSync("index.html"));
+
+  response.send(bufIndex.toString());
 });
 
 var port = process.env.PORT || 5000;
