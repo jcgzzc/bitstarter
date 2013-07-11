8904477afee8927e89e587df1e426f1394b82eea
Jose Gonzalez <jcgzzc@gmail.com>
2013-07-11 21:58:08 +0000

    changed main page to call index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var bufIndex = fs.readFileSync("index.html");
+  var strIndex = bufIndex.toString();
+
+  response.send();
 });
 
 var port = process.env.PORT || 5000;
